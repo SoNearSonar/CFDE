a4a9edfcda7ba6274da3aeb6643b0884c88bd8e6
SoNearSonar <sonearsonar@gmail.com>
2023-05-12 17:53:52 -0400

    Add files for current state of project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CFDE
-Compressed File Downloader and Extractor. Python script that downloads .zip, .tar.gz, and .7z files and extracts them
+# Compressed File Downloader and Extractor
+A Python script that downloads .zip, .tar.gz, and .7z files from the web and extracts them
---
+++ cfde.py
@@ -0,0 +1,47 @@
+import re, os, wget
+from tkinter import filedialog as fd
+from zipfile import ZipFile as zf
+from tarfile import TarFile as tf
+from py7zr import SevenZipFile as sz
+
+# Credit to https://github.com/django/django/blob/stable/1.3.x/django/core/validators.py for the first regex
+regexUrl = re.compile(r'^(?:http|ftp)s?://', re.IGNORECASE)
+
+# Credit to https://www.geeksforgeeks.org/python-program-to-find-files-having-a-particular-extension-using-regex/# for the next regex lines
+regexZipFileType = re.compile('\.zip$', re.IGNORECASE)
+regex7ZipFileType = re.compile('\.7z$', re.IGNORECASE)
+regexTarFileType = re.compile('\.(tgz$)|(tar\.gz$)|(tar\.xz$)', re.IGNORECASE)
+
+print('Enter in the file url (.zip) here that you want to download: ')
+fileUrl = input()
+
+zipFileMatch = re.search(regexZipFileType, fileUrl)
+tarFileMatch = re.search(regexTarFileType, fileUrl)
+sevenZipFileMatch = re.search(regex7ZipFileType, fileUrl)
+urlMatch = re.search(regexUrl, fileUrl)
+
+if (not zipFileMatch and not tarFileMatch and not sevenZipFileMatch) or not urlMatch:
+    print('\nWhat was entered is not a valid url')
+    quit()
+else:
+    print('\nEnter in the directory you want the file to be downloaded to (a window will pop up)')
+    directoryName = fd.askdirectory()
+    
+    if not os.path.isdir(directoryName):
+        print('\nWhat was entered is not a valid directory')
+        quit()
+    
+    fileName = wget.download(fileUrl, directoryName)
+
+    if zipFileMatch:
+        zf(fileName, 'r').extractall(directoryName)
+    elif tarFileMatch:
+        with tf.open(fileName) as t:
+            t.extractall(directoryName)
+    elif sevenZipFileMatch:
+        try:
+            sz(fileName, mode = 'r').extractall(directoryName)
+        except:
+            print('\n\nThis .7z file cannot be processed (The header maybe is not supported by Python)')
+    
+    quit()
