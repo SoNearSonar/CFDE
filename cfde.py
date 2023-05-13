e38e22d070074189d28fc16b201c2de791fecf2a
SoNearSonar <sonearsonar@gmail.com>
2023-05-12 23:36:56 -0400

    Add more exception catching
    And some small code cleanups

--- cfde.py
+++ cfde.py
@@ -12,7 +12,7 @@ regexZipFileType = re.compile('\.zip$', re.IGNORECASE)
 regex7ZipFileType = re.compile('\.7z$', re.IGNORECASE)
 regexTarFileType = re.compile('\.(tgz$)|(tar\.gz$)|(tar\.xz$)', re.IGNORECASE)
 
-print('Enter in the file url (.zip) here that you want to download: ')
+print('Enter in the file url here that you want to download: ')
 fileUrl = input()
 
 zipFileMatch = re.search(regexZipFileType, fileUrl)
@@ -22,26 +22,24 @@ urlMatch = re.search(regexUrl, fileUrl)
 
 if (not zipFileMatch and not tarFileMatch and not sevenZipFileMatch) or not urlMatch:
     print('\nWhat was entered is not a valid url')
-    quit()
 else:
-    print('\nEnter in the directory you want the file to be downloaded to (a window will pop up)')
-    directoryName = fd.askdirectory()
-    
-    if not os.path.isdir(directoryName):
-        print('\nWhat was entered is not a valid directory')
-        quit()
-    
-    fileName = wget.download(fileUrl, directoryName)
+    try:
+        print('\nEnter in the directory you want the file to be downloaded to (a window will pop up)')
+        directoryName = fd.askdirectory()
+        
+        if not os.path.isdir(directoryName):
+            print('\nWhat was entered is not a valid directory')
+            quit()
+        
+        fileName = wget.download(fileUrl, directoryName)
 
-    if zipFileMatch:
-        zf(fileName, 'r').extractall(directoryName)
-    elif tarFileMatch:
-        with tf.open(fileName) as t:
-            t.extractall(directoryName)
-    elif sevenZipFileMatch:
-        try:
+        if zipFileMatch:
+            zf(fileName, 'r').extractall(directoryName)
+        elif tarFileMatch:
+            with tf.open(fileName) as t:
+                t.extractall(directoryName)
+        elif sevenZipFileMatch:
             sz(fileName, mode = 'r').extractall(directoryName)
-        except:
-            print('\n\nThis .7z file cannot be processed (The header maybe is not supported by Python)')
-    
-    quit()
+    except Exception as ex:
+        print('An error occurred when using this program: ' + ex)
+quit()
